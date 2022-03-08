548ca12908197bb1d9e7493396d64e954a6bba9f
Devesh225 <deveshtulshyan@gmail.com>
2022-03-08 13:31:18 +0530

    Id Changes for Linking the Components of the Webpage

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,8 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
+import './Navbar.css';
+import { AiOutlineHome } from 'react-icons/ai';
+import { AiOutlineUser } from 'react-icons/ai';
+import { GiSkills } from 'react-icons/gi';
+import { RiCustomerService2Line } from 'react-icons/ri';
+import { AiOutlineContacts } from 'react-icons/ai';
 
 function Navbar() {
+  const [activeLink, setActiveLink] = useState('#header');
   return (
-    <div>Navbar</div>
+    <div className='navbar'>
+      <a href="#header" onClick={() => setActiveLink('#header')} className={activeLink === '#header' ? 'active' : ''}><AiOutlineHome /></a>
+      <a href="#about" onClick={() => setActiveLink('#about')} className={activeLink === '#about' ? 'active' : ''}><AiOutlineUser /></a>
+      <a href="#skills" onClick={() => setActiveLink('#skills')} className={activeLink === '#skills' ? 'active' : ''}><GiSkills /></a>
+      <a href="#services" onClick={() => setActiveLink('#services')} className={activeLink === '#services' ? 'active' : ''}><RiCustomerService2Line /></a>
+      <a href="#contact" onClick={() => setActiveLink('#contact')} className={activeLink === '#contact' ? 'active' : ''}><AiOutlineContacts /></a>
+    </div>
   )
 }
 
