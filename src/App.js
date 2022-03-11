8359f17682a5b5ae810ae5b9ea22aa2efb029f86
Devesh225 <deveshtulshyan@gmail.com>
2022-03-12 01:13:00 +0530

    Skill component Rendering

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Fragment } from 'react'
 import About from './components/about/About';
 import Header from './components/header/Header';
 import Navbar from './components/navbar/Navbar';
+import Skills from './components/skills/Skills';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Header />
       <Navbar />
       <About />
+      <Skills />
     </Fragment>
   )
 }
