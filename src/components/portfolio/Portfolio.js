43bd812d94b3e09bd2aa7a54e01a5ac82e8d8e45
Devesh225 <deveshtulshyan@gmail.com>
2022-03-13 01:17:31 +0530

    Portfolio Component, Responsive UI and all functionality added

---
+++ src/components/portfolio/Portfolio.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import './Portfolio.css';
+import LinkedIn from '../../assets/linkedin.png';
+import GoogleKeep from '../../assets/googlekeep.png'
+
+function Portfolio() {
+
+  const portfolioItems = [
+    {
+      PID: 1,
+      imageLink: LinkedIn,
+      title: "LinkedIn Clone",
+      githubLink: "https://github.com/Devesh225/Linkedin-Clone",
+      liveLink: "https://linkedin-clone-devesh.web.app/", 
+    },
+    {
+      PID: 2,
+      imageLink: GoogleKeep,
+      title: "Google Keep Clone",
+      githubLink: "https://github.com/Devesh225/Note-Keeper",
+      liveLink: "http://devesh225.github.io/Note-Keeper",
+    } 
+  ];
+
+  return (
+    <div id='portfolio' className='portfolio'>
+        <h1>My Portfolio</h1>
+        <h4>Recent Projects</h4>
+        <div className="container portfolio__container">
+          {
+            portfolioItems.map(({PID, imageLink, title, githubLink, liveLink}) => {
+              return (
+                <div key={PID} className="portfolio__card">
+                  <div className="portfolio__projectImage">
+                  <img src={imageLink} alt="LinkedIn Screenshot" />
+                  </div>
+                  <div className="portfolio__projectContent">
+                    <h3>{title}</h3>
+                    <div className="portfolio__projectButtons">
+                      <a target='_blank' className='btn btn1' href={githubLink}>Github Repo</a>
+                      <a target='_blank' className='btn btn-primary btn2' href={liveLink}>Live Demo</a>
+                    </div>
+                  </div>
+                </div>
+              )
+            })
+          }
+        </div>
+    </div>
+  )
+}
+
+export default Portfolio
